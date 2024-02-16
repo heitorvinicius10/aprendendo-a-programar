e6dca2ff5281614505c90781a579fca1b72af98c
Heitor Vinicius <160193273+heitorvinicius10@users.noreply.github.com>
2024-02-15 21:42:04 -0300

    Add files via upload

---
+++ calculos.py
@@ -0,0 +1,21 @@
+#meu primeiro programa
+
+print ("calculadora menos")
+primeiro = 302
+segundo = 201
+print (primeiro-segundo)
+
+print("calculadora mais")
+primeiro1 = 4011
+segundo1 = 3020
+print (primeiro1+segundo1)
+
+print ("calculadora vezes")
+primeiro2 = 401
+segundo2 = 6
+print (primeiro2*segundo2)
+
+print ("calculadora divisão")
+primeiro3 = 2201 
+segundo3 = 44
+print (primeiro3/segundo3)
